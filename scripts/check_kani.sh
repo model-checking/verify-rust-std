97dfc23cf647276387daaf8a3041402948fcf9b4
Jaisurya Nanduri <jaisnan@amazon.com>
2024-07-26 16:36:35 +0000

    test subtree sync workflow
    add pull_request just for testing
    
    add push permission for testing
    
    Test without updating subtree
    
    Try within kani dir
    
    Use kani as workdir
    
    Change parsing
    
    Parse regex with path
    
    Check for toolchain file
    
    Get toolchain date
    
    Use correct hash and date
    
    Use commit hash instead
    
    make upstream pull explicit
    
    Add origin for pulling
    
    set working directory to head
    
    Fix working directory
    
    Update subtree
    
    Add --quiet
    
    Update with --quiet
    
    Add pull_from_upstream.sh
    
    Add scripts -1
    
    Add explicit commit pull
    
    Add temp home dir
    
    Add clone from temp
    
    Add base repo
    
    Update comments
    
    clone into tempdir
    
    Add working tests

---
+++ scripts/check_kani.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Set the working directories
+VERIFY_RUST_STD_DIR="$1"
+KANI_DIR=$(mktemp -d)
+
+RUNNER_TEMP=$(mktemp -d)
+
+# Checkout your local repository
+echo "Checking out local repository..."
+echo
+cd "$VERIFY_RUST_STD_DIR"
+
+# Checkout the Kani repository
+echo "Checking out Kani repository..."
+echo
+git clone --depth 1 -b features/verify-rust-std https://github.com/model-checking/kani.git "$KANI_DIR"
+
+# Setup dependencies for Kani
+echo "Setting up dependencies for Kani..."
+echo
+cd "$KANI_DIR"
+./scripts/setup/ubuntu/install_deps.sh
+
+# Build Kani
+echo "Building Kani..."
+echo
+cargo build-dev --release
+# echo "$(pwd)/scripts" >> $PATH
+
+# Run tests
+echo "Running tests..."
+echo
+cd "$VERIFY_RUST_STD_DIR"
+$KANI_DIR/scripts/kani verify-std -Z unstable-options /tmp/verify-rust-std/library --target-dir "$RUNNER_TEMP" -Z function-contracts -Z mem-predicates -Z ptr-to-ref-cast-checks
+
+echo "Tests completed."
+echo
+
+# Clean up the Kani directory (optional)
+rm -rf "$KANI_DIR"
+rm -rf "$VERIFY_RUST_STD_DIR"
