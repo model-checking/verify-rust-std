0a008d6fad75524c4aa7d947be4175868d58c46a
Carolyn Zech <cmzech@amazon.com>
2024-09-18 12:56:14 -0400

    use self to refer to fields

--- library/contracts/safety/src/lib.rs
+++ library/contracts/safety/src/lib.rs
@@ -20,9 +20,7 @@ mod tool;
 pub fn invariant(attr: TokenStream, item: TokenStream) -> TokenStream {
     let safe_body = proc_macro2::TokenStream::from(attr);
     let item = parse_macro_input!(item as ItemStruct);
-    let mut field_refs = field_refs_inner(&item.fields);
     let mut item_name = &item.ident;
-
     let (impl_generics, ty_generics, where_clause) = item.generics.split_for_impl();
 
     let expanded = quote! {
@@ -30,8 +28,6 @@ pub fn invariant(attr: TokenStream, item: TokenStream) -> TokenStream {
         #[unstable(feature="invariant", issue="none")]
         impl #impl_generics core::ub_checks::Invariant for #item_name #ty_generics #where_clause {
             fn is_safe(&self) -> bool {
-                let obj = self;
-                #field_refs
                 #safe_body
             }
         }
@@ -50,30 +46,4 @@ pub fn requires(attr: TokenStream, item: TokenStream) -> TokenStream {
 #[proc_macro_attribute]
 pub fn ensures(attr: TokenStream, item: TokenStream) -> TokenStream {
     tool::ensures(attr, item)
-}
-
-/// Generates the sequence of expressions to initialize the variables used as
-/// references to the struct fields.
-fn field_refs_inner(fields: &Fields) -> proc_macro2::TokenStream {
-    match fields {
-        Fields::Named(ref fields) => {
-            let field_refs: Vec<proc_macro2::TokenStream> = fields
-                .named
-                .iter()
-                .map(|field| {
-                    let name = &field.ident;
-                    quote_spanned! {field.span()=>
-                        let #name = &obj.#name;
-                    }
-                })
-                .collect();
-            if !field_refs.is_empty() {
-                quote! { #( #field_refs )* }
-            } else {
-                quote! {}
-            }
-        }
-        Fields::Unnamed(_) => quote! {},
-        Fields::Unit => quote! {},
-    }
 }
