16a9a86bff2876a3484c9e7bab123215fb1922db
Celina G. Val <celinval@amazon.com>
2024-12-03 17:01:13 -0800

    Run Rust's tidy script
    Ensure that everything is properly formatted

--- scripts/check_rustc.sh
+++ scripts/check_rustc.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
+# Runs the Rust bootstrap script in order to ensure the changes to this repo
+# is compliant with the Rust repository tests.
+#
+# TODO: Need to enable full tidy run.
 
-set -e
+set -eu
+
+usage() {
+    echo "Usage: $0 [--help] [--bless]"
+    echo "Options:"
+    echo "  -h, --help      Show this help message"
+    echo "  --bless         Update library files using tidy"
+}
+
+TIDY_MODE=""
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -h|--help)
+            usage
+            exit 0
+            ;;
+        --bless)
+            TIDY_MODE="--bless"
+            shift 1
+            ;;
+        *)
+            echo "Error: Unknown option `$1`"
+            usage
+            exit 1
+            ;;
+    esac
+done
 
 # Set the working directory for your local repository
-HEAD_DIR=$1
+HEAD_DIR=$(git rev-parse --show-toplevel)
 
 # Temporary directory for upstream repository
-TEMP_DIR=$(mktemp -d)
+RUST_DIR=$(mktemp -d --suffix ".rust")
 
 # Checkout your local repository
 echo "Checking out local repository..."
@@ -17,28 +48,54 @@ echo "Retrieving commit ID..."
 COMMIT_ID=$(rustc --version | sed -e "s/.*(\(.*\) .*/\1/")
 echo "$COMMIT_ID for rustc is"
 
-# Clone the rust-lang/rust repository
-echo "Cloning rust-lang/rust repository..."
-git clone https://github.com/rust-lang/rust.git "$TEMP_DIR/upstream"
+# Get the full commit ID for shallow clone
+curl -H "Connection: close" -o "${RUST_DIR}/output.json" -s --show-error \
+    "https://api.github.com/repos/rust-lang/rust/commits?sha=${COMMIT_ID}&per_page=1"
+COMMIT_ID=$(jq -r '.[0].sha' "${RUST_DIR}/output.json")
 
-# Checkout the specific commit
+# Clone the rust-lang/rust repository
+echo "Cloning rust-lang/rust repository into ${RUST_DIR}..."
+pushd "$RUST_DIR" > /dev/null
+git init
+git remote add origin https://github.com/rust-lang/rust.git
+git fetch --depth 1 origin $COMMIT_ID
+
 echo "Checking out commit $COMMIT_ID..."
-cd "$TEMP_DIR/upstream"
 git checkout "$COMMIT_ID"
+git submodule update --init --depth 1
+popd
 
 # Copy your library to the upstream directory
 echo "Copying library to upstream directory..."
-rm -rf "$TEMP_DIR/upstream/library"
-cp -r "$HEAD_DIR/library" "$TEMP_DIR/upstream"
+rm -rf "${RUST_DIR}/library"
+cp -r "${HEAD_DIR}/library" "${RUST_DIR}"
 
-# Run the tests
-cd "$TEMP_DIR/upstream"
-export RUSTFLAGS="--check-cfg cfg(kani) --check-cfg cfg(feature,values(any()))"
-echo "Running tests..."
+# Configure repository
+pushd "${RUST_DIR}"
 ./configure --set=llvm.download-ci-llvm=true
+export RUSTFLAGS="--check-cfg cfg(kani) --check-cfg cfg(feature,values(any()))"
+
+# Run tidy
+if [ "${TIDY_MODE}" == "--bless" ];
+then
+    echo "Run rustfmt"
+    # TODO: This should be:
+    # ./x test tidy --bless
+    ./x fmt
+    cp -r "${RUST_DIR}/library" "${HEAD_DIR}"
+else
+    # TODO: This should be:
+    # ./x test tidy
+    echo "Check format"
+    ./x fmt --check
+fi
+
+# Run tests
+cd "$RUST_DIR/upstream"
+echo "Running tests..."
 ./x test --stage 0 library/std
 
 echo "Tests completed."
 
 # Clean up the temporary directory
-rm -rf "$TEMP_DIR"
+rm -rf "$RUST_DIR"
