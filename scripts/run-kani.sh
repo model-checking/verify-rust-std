a8b90786560ab7b8248f4119b017ab86dc9d0c34
Michael Tautschnig <tautschn@amazon.com>
2025-04-28 16:33:20 +0000

    Limit clone depth, add new per-crate option

--- scripts/run-kani.sh
+++ scripts/run-kani.sh
@@ -334,9 +334,9 @@ main() {
         ./std-analysis.sh $build_dir
         popd
         echo "Running autoharness-analyzer command..."
-        git clone https://github.com/carolynzech/autoharness_analyzer
+        git clone --depth 1 https://github.com/carolynzech/autoharness_analyzer
         cd autoharness_analyzer
-        cargo run \
+        cargo run --per-crate \
           ../target/kani_verify_std/target/x86_64-unknown-linux-gnu/debug/deps/ \
           /tmp/std_lib_analysis/results/
     fi
