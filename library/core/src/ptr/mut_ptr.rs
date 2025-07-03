4ea1d2719e502674173fd46269bf15360dc7d1b4
Michael Tautschnig <tautschn@amazon.com>
2025-07-03 16:09:56 +0000

    Whitespace fixes

--- library/core/src/ptr/mut_ptr.rs
+++ library/core/src/ptr/mut_ptr.rs
@@ -1097,8 +1097,8 @@ impl<T: PointeeSized> *mut T {
     // #[requires(
     //     // If count is zero, any pointer is valid including null pointer.
     //     (count == 0) ||
-    //     // Else if count is not zero, then ensure that adding `count` doesn't cause 
-    //     // overflow and that both pointers `self` and the result are in the same 
+    //     // Else if count is not zero, then ensure that adding `count` doesn't cause
+    //     // overflow and that both pointers `self` and the result are in the same
     //     // allocation
     //     (
     //         (count <= isize::MAX as usize) &&
@@ -1243,8 +1243,8 @@ impl<T: PointeeSized> *mut T {
     // #[requires(
     //     // If count is zero, any pointer is valid including null pointer.
     //     (count == 0) ||
-    //     // Else if count is not zero, then ensure that subtracting `count` doesn't 
-    //     // cause overflow and that both pointers `self` and the result are in the 
+    //     // Else if count is not zero, then ensure that subtracting `count` doesn't
+    //     // cause overflow and that both pointers `self` and the result are in the
     //     // same allocation.
     //     (
     //         (count <= isize::MAX as usize) &&
