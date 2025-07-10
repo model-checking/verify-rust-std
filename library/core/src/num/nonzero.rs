3f6b2e1ed5ce85e5df46933b30e3243bc467df5e
Michael Tautschnig <tautschn@amazon.com>
2025-07-10 16:33:40 +0000

    Fmt

--- library/core/src/num/nonzero.rs
+++ library/core/src/num/nonzero.rs
@@ -2414,18 +2414,66 @@ mod verify {
     }
 
     // Generate harnesses for multiple types
-    nonzero_check_from_mut_unchecked!(i8, core::num::NonZeroI8, nonzero_check_from_mut_unchecked_i8);
-    nonzero_check_from_mut_unchecked!(i16, core::num::NonZeroI16, nonzero_check_from_mut_unchecked_i16);
-    nonzero_check_from_mut_unchecked!(i32, core::num::NonZeroI32, nonzero_check_from_mut_unchecked_i32);
-    nonzero_check_from_mut_unchecked!(i64, core::num::NonZeroI64, nonzero_check_from_mut_unchecked_i64);
-    nonzero_check_from_mut_unchecked!(i128, core::num::NonZeroI128, nonzero_check_from_mut_unchecked_i128);
-    nonzero_check_from_mut_unchecked!(isize, core::num::NonZeroIsize, nonzero_check_from_mut_unchecked_isize);
-    nonzero_check_from_mut_unchecked!(u8, core::num::NonZeroU8, nonzero_check_from_mut_unchecked_u8);
-    nonzero_check_from_mut_unchecked!(u16, core::num::NonZeroU16, nonzero_check_from_mut_unchecked_u16);
-    nonzero_check_from_mut_unchecked!(u32, core::num::NonZeroU32, nonzero_check_from_mut_unchecked_u32);
-    nonzero_check_from_mut_unchecked!(u64, core::num::NonZeroU64, nonzero_check_from_mut_unchecked_u64);
-    nonzero_check_from_mut_unchecked!(u128, core::num::NonZeroU128, nonzero_check_from_mut_unchecked_u128);
-    nonzero_check_from_mut_unchecked!(usize, core::num::NonZeroUsize, nonzero_check_from_mut_unchecked_usize);
+    nonzero_check_from_mut_unchecked!(
+        i8,
+        core::num::NonZeroI8,
+        nonzero_check_from_mut_unchecked_i8
+    );
+    nonzero_check_from_mut_unchecked!(
+        i16,
+        core::num::NonZeroI16,
+        nonzero_check_from_mut_unchecked_i16
+    );
+    nonzero_check_from_mut_unchecked!(
+        i32,
+        core::num::NonZeroI32,
+        nonzero_check_from_mut_unchecked_i32
+    );
+    nonzero_check_from_mut_unchecked!(
+        i64,
+        core::num::NonZeroI64,
+        nonzero_check_from_mut_unchecked_i64
+    );
+    nonzero_check_from_mut_unchecked!(
+        i128,
+        core::num::NonZeroI128,
+        nonzero_check_from_mut_unchecked_i128
+    );
+    nonzero_check_from_mut_unchecked!(
+        isize,
+        core::num::NonZeroIsize,
+        nonzero_check_from_mut_unchecked_isize
+    );
+    nonzero_check_from_mut_unchecked!(
+        u8,
+        core::num::NonZeroU8,
+        nonzero_check_from_mut_unchecked_u8
+    );
+    nonzero_check_from_mut_unchecked!(
+        u16,
+        core::num::NonZeroU16,
+        nonzero_check_from_mut_unchecked_u16
+    );
+    nonzero_check_from_mut_unchecked!(
+        u32,
+        core::num::NonZeroU32,
+        nonzero_check_from_mut_unchecked_u32
+    );
+    nonzero_check_from_mut_unchecked!(
+        u64,
+        core::num::NonZeroU64,
+        nonzero_check_from_mut_unchecked_u64
+    );
+    nonzero_check_from_mut_unchecked!(
+        u128,
+        core::num::NonZeroU128,
+        nonzero_check_from_mut_unchecked_u128
+    );
+    nonzero_check_from_mut_unchecked!(
+        usize,
+        core::num::NonZeroUsize,
+        nonzero_check_from_mut_unchecked_usize
+    );
 
     macro_rules! nonzero_check_cmp {
         ($nonzero_type:ty, $nonzero_check_cmp_for:ident) => {
