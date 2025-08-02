klausz65/rust

corrected lifetime in core::panic::Location::file return type

library/core/src/panic/location.rs
@@ -126,7 +126,7 @@ impl<'a> Location<'a> {
     #[stable(feature = "panic_hooks", since = "1.10.0")]
     #[rustc_const_stable(feature = "const_location_fields", since = "1.79.0")]
     #[inline]
-    pub const fn file(&self) -> &str {
+    pub const fn file(&self) -> &'a str {
         self.file
     }
 

model-checking/verify-rust-std

core/src/panic/location.rs
@@ -195,7 +195,7 @@ impl<'a> Location<'a> {
     #[must_use]
     #[unstable(feature = "file_with_nul", issue = "141727")]
     #[inline]
-    pub const fn file_with_nul(&self) -> &CStr {
+    pub const fn file_with_nul(&self) -> &'a CStr {
         let filename = self.filename.as_ptr();
 
         // SAFETY: The filename is valid for `filename_len+1` bytes, so this addition can't