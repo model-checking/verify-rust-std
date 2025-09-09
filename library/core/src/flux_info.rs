0375ccda1766b1e20b31a7524d0bb9c5516d99f2
Nico Lehmann <nico.lehmannm@gmail.com>
2025-09-09 11:37:29 -0300

    Add refinements to num/mod.rs

--- library/core/src/flux_info.rs
+++ library/core/src/flux_info.rs
@@ -19,6 +19,39 @@
     property ShiftLeft[<<](n, k) {
         0 < k => n <= [<<](n, k)
     }
+
+    fn is_ascii_uppercase(n: int) -> bool {
+        cast('A') <= n && n <= cast('Z')
+    }
+
+    fn is_ascii_lowercase(n: int) -> bool {
+        cast('a') <= n && n <= cast('z')
+    }
+
+    fn to_ascii_uppercase(n: int) -> int {
+        n - (cast(is_ascii_lowercase(n)) * 32)
+    }
+
+    fn to_ascii_lowercase(n: int) -> int {
+        n + (cast(is_ascii_uppercase(n)) * 32)
+    }
+
+    property BitXor0[^](x, y) {
+        (y == 0) => [^](x, y) == x
+    }
+
+    property BitXor32[^](x, y) {
+        (is_ascii_lowercase(x) && y == 32) => [^](x, y) == x - 32
+    }
+
+    property BitOr0[|](x, y) {
+        (y == 0) => [|](x, y) == x
+    }
+
+    property BitOr32[|](x, y) {
+        (is_ascii_uppercase(x) && y == 32) => [|](x, y) == x + 32
+    }
+
 }]
 #[flux::specs {
     mod hash {
@@ -50,7 +83,7 @@
     }
 
     impl Debug for time::Duration {
-        #[trusted(reason="modular arithmetic invariant inside nested fmt_decimal")]         
+        #[trusted(reason="modular arithmetic invariant inside nested fmt_decimal")]
         fn fmt(self: &Self, f: &mut fmt::Formatter) -> fmt::Result;
     }
 }]
