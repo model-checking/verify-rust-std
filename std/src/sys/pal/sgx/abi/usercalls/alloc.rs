rust-lang/rust

std::sys::pal::sgx: fix mismatched alloc/free alignment
`User::new_uninit_bytes` and `User::drop` are asking the host to
alloc/dealloc memory with potentially mismatched alignment, as the
enclave side is unconditionally over-aligning on allocation but not
doing the same on free.

- Ex: `User::<ByteBuffer>` -> `alloc(_, align=8)` -> `drop()` -> `free(_, align=1)`

For most hosts running stock x86_64-linux + glibc malloc, I don't
believe this mismatch is an issue, since posix `free` ignores the
alignment anyway. My guess is that if you're using jemalloc, which does
care about the dealloc alignment, then something _might_ go wrong.

It's also not clear that we can just round-up the alignment on `free`,
since `User::from_raw` exists, and there's various places that call it
outside std.

We should probably just remove the min. alignment until we come up with
a more satisfactory solution. My guess is that the right place to do the
min. alignment optimization is in the host-side enclave-runner:
<https://github.com/fortanix/rust-sgx/blob/be93e7abe92eff4b5610e15fe21b16196ace1e6e/intel-sgx/enclave-runner-sgx/src/usercalls/mod.rs#L1596>
and other places that hand memory to the SGX enclave.

NB. The min. alignment exists for performance reasons (see:
`copy_from_userspace`). It's highly preferable if all memory copied from
userspace is at least 8 byte aligned, otherwise we have to fallback to a
super slow copy routine for the unaligned prefix (and suffix).

library/std/src/sys/pal/sgx/abi/usercalls/alloc.rs
@@ -253,11 +253,9 @@ where
         unsafe {
             // Mustn't call alloc with size 0.
             let ptr = if size > 0 {
-                // `copy_to_userspace` is more efficient when data is 8-byte aligned
-                let alignment = cmp::max(T::align_of(), 8);
-                rtunwrap!(Ok, super::alloc(size, alignment)) as _
+                rtunwrap!(Ok, super::alloc(size, T::align_of())) as _
             } else {
-                T::align_of() as _ // dangling pointer ok for size 0
+                crate::ptr::dangling() // dangling pointer ok for size 0
             };
             if let Ok(v) = crate::panic::catch_unwind(|| T::from_raw_sized(ptr, size)) {
                 User(NonNull::new_userref(v))

model-checking/verify-rust-std

std/src/sys/pal/sgx/abi/usercalls/alloc.rs
@@ -253,11 +253,9 @@ where
         unsafe {
             // Mustn't call alloc with size 0.
             let ptr = if size > 0 {
-                // `copy_to_userspace` is more efficient when data is 8-byte aligned
-                let alignment = cmp::max(T::align_of(), 8);
-                rtunwrap!(Ok, super::alloc(size, alignment)) as _
+                rtunwrap!(Ok, super::alloc(size, T::align_of())) as _
             } else {
-                T::align_of() as _ // dangling pointer ok for size 0
+                crate::ptr::dangling_mut() // dangling pointer ok for size 0
             };
             if let Ok(v) = crate::panic::catch_unwind(|| T::from_raw_sized(ptr, size)) {
                 User(NonNull::new_userref(v))