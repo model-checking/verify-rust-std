3641c2b93f7769f924791848d1afcae22f185816
Onyeka Obi <softwareengineerasaservant@isurvivable.cv>
2026-09-13 01:15:16 -0700

    Account for reference provenance in array regression contracts
    Signed-off-by: Onyeka Obi <softwareengineerasaservant@isurvivable.cv>

--- verifast-proofs/core/iter/adapters/backend/array-invalid.rs
+++ verifast-proofs/core/iter/adapters/backend/array-invalid.rs
@@ -2,9 +2,10 @@
 #![crate_name = "array_invalid"]
 
 unsafe fn shared<'a, T, const N: usize>(p: *const [T; N]) -> &'a [T; N]
-//@ req thread_token(?t) &*& [?q]lifetime_token('a);
-//@ ens thread_token(t) &*& [q]lifetime_token('a) &*& result == p;
+//@ req thread_token(?t) &*& [?q]lifetime_token('a) &*& [?r]ref_initialized(p);
+//@ ens thread_token(t) &*& [q]lifetime_token('a) &*& result == p &*& [_](<[T; N]>.share)('a, t, result) &*& [r]ref_initialized(p);
 //@ on_unwind_ens false;
 {
+    //@ reborrow_ref_(p);
     unsafe { &*p }
 }
--- verifast-proofs/core/iter/adapters/backend/array-valid.rs
+++ verifast-proofs/core/iter/adapters/backend/array-valid.rs
@@ -2,16 +2,17 @@
 #![crate_name = "array_valid"]
 
 unsafe fn shared<'a, T, const N: usize>(p: *const [T; N]) -> &'a [T; N]
-//@ req thread_token(?t) &*& [?q]lifetime_token('a) &*& [_](<[T; N]>.share)('a, t, p);
-//@ ens thread_token(t) &*& [q]lifetime_token('a) &*& result == p &*& [_](<[T; N]>.share)('a, t, result);
+//@ req thread_token(?t) &*& [?q]lifetime_token('a) &*& [_](<[T; N]>.share)('a, t, p) &*& [?r]ref_initialized(p);
+//@ ens thread_token(t) &*& [q]lifetime_token('a) &*& result == p &*& [_](<[T; N]>.share)('a, t, result) &*& [r]ref_initialized(p);
 //@ on_unwind_ens false;
 {
+    //@ reborrow_ref_(p);
     unsafe { &*p }
 }
 
 unsafe fn exclusive<'a, T, const N: usize>(p: *mut [T; N]) -> &'a mut [T; N]
-//@ req thread_token(?t) &*& [?q]lifetime_token('a) &*& full_borrow('a, <[T; N]>.full_borrow_content(t, p));
-//@ ens thread_token(t) &*& [q]lifetime_token('a) &*& result == p &*& full_borrow('a, <[T; N]>.full_borrow_content(t, result));
+//@ req *p |-> ?values;
+//@ ens *result |-> values &*& ref_mut_end_token(result, p);
 //@ on_unwind_ens false;
 {
     unsafe { &mut *p }
